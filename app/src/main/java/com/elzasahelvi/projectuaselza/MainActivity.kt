a8932e64ab38b999770a7cef7288e7cde4264938
Fajar22-Dev <FAJAR HIDAYAT>
2024-07-09 22:01:48 +0700

    Initial commit

---
+++ app/src/main/java/com/elzasahelvi/projectuaselza/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.elzasahelvi.projectuaselza
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.elzasahelvi.projectuaselza.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val movieFragment = MovieFragment()
+        val tvFragment = TVFragment()
+        val profileFragment = ProfileFragment()
+
+        makeCurrentFragment (movieFragment)
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_movie -> makeCurrentFragment(movieFragment)
+                R.id.menu_tv -> makeCurrentFragment(tvFragment)
+                R.id.menu_profile -> makeCurrentFragment(profileFragment)
+            }
+
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fram_nav, fragment)
+            commit()
+        }
+
+    }
+}
